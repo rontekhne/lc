67f8502c237d027e822749c17fbe4695b10ebdda
ron <rontekhne@users.noreply.github.com>
2021-06-13 11:28:44 -0300

    Finalizado o Capítulo 01.

---
+++ 01-Intro/Comentarios/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+    A função printf()
+    serve para
+    escrever na tela
+    */
+    printf("Hello World \n");
+    // faz uma pausa no programa; específico para MSDOS
+    // system("pause");
+    return 0;
+}
